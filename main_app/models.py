fb014c946a84261cd559da020d393548590a388d
Akil92 <akilmaynard@yahoo.com>
2023-08-13 16:58:44 -0400

    superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 
 class Train(models.Model):
@@ -8,22 +8,25 @@ class Train(models.Model):
     cars = models.IntegerField()
     capacity = models.IntegerField()
     rating = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default = 2)
 
 class Route(models.Model):
     name = models.CharField(max_length= 50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default = 2)
 
 class Schedule(models.Model):
     train = models.ForeignKey(
         Train,
-        on_delete= models.CASCADE
+        on_delete= models.CASCADE, default = 2
     )
     route = models.ForeignKey(
         Route, 
-        on_delete= models.CASCADE
+        on_delete= models.CASCADE, default = 2
     )
     departure_datetime = models.DateField('Departure Date')
     arrival_datetime = models.DateField('Arrival Date')
     base_fare = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default = 2)
 
 
 
@@ -31,22 +34,25 @@ class Schedule(models.Model):
 class Booking(models.Model):
     schedule = models.ForeignKey(
         Schedule, 
-        on_delete = models.CASCADE
+        on_delete = models.CASCADE, default = 2
     )
     num_passengers = models.IntegerField()
     fare = models.FloatField()
     luggage_weight = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default = 2)
 
 class Destination(models.Model):
     name = models.CharField()
     city = models.CharField()
     state = models.CharField()
     Country = models.CharField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default = 2)
 
 
 class Comment(models.Model):
     content = models.CharField()
     rating = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default = 2)
  
 
 
