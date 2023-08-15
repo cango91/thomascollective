477a3ef7c4ad6a4155e69fb66b5eb0a963118443
Ashley Akins <ashleyakins2015@hotmail.com>
2023-08-15 09:09:48 -0500

    comments

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,14 @@
 from django.forms import ModelForm
 
-from .models import Train
+from .models import Train, Comment
 
 
 class TrainForm(ModelForm):
     class Meta:
-        model = train
+        model = Train
         fields = '__all__'
+
+class CommentForm(ModelForm):
+  class Meta:
+    model = Comment
+    fields = ['content', 'rating']
