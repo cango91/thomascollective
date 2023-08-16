e711bf52239a1add7f5b3d872f71d06b087c6d49
cango91 <can.gologlu@gmail.com>
2023-08-16 09:29:13 -0400

    STAR SELECTORgit add .

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,14 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Train, Comment
 
 
-class TrainForm(ModelForm):
+class TrainForm(forms.ModelForm):
     class Meta:
         model = Train
         fields = '__all__'
 
-class CommentForm(ModelForm):
-  class Meta:
-    model = Comment
-    fields = ['content', 'rating']
+class CommentForm(forms.ModelForm):
+    rating = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'star-rating star-rating-editable'}))
+    class Meta:
+        model = Comment
+        fields = ['content', 'rating']
