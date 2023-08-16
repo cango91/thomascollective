92f9e5bd679e917547274fc35e6f91197def2533
Ashley Akins <ashleyakins2015@hotmail.com>
2023-08-16 15:45:33 -0500

    journey stuff

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseForbidden
 from django.utils.timezone import now
-from .models import Train, Route, Booking, Comment
+from .models import Train, Route, Booking, Comment, Journey
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -14,7 +14,7 @@ def home(request):
     return render(request, 'home.html')
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
 
 
 class TrainCreate(CreateView):
@@ -32,10 +32,10 @@ def train_index(request):
 
 def train_detail(request, train_id):
     train = Train.objects.get(id=train_id)
-    Comments = train.comment_set.all()
+    comments = train.comment_set.all()
     form = CommentForm()
 
-    return render(request, 'train/train_detail.html', {'train': train, 'form': form})
+    return render(request, 'train/train_detail.html', {'train': train, 'comments': comments, 'form': form})
 
 
 class CommentUpdate(UpdateView):
@@ -46,27 +46,26 @@ class CommentUpdate(UpdateView):
 
 def update_comment(request, pk):
     if request.method == 'POST':
-      form = CommentForm(request.POST)
-      #comment = form.save(commit=False)
-      comment = get_object_or_404(Comment, id=pk)
-      if form.is_valid():
-        if not comment.user == request.user:
-          error_msg = "You are not allowed to edit this comment, because it does not belong to you"
-          return render(request, 'comment/edit_comment.html', {'form': form, 'comment': comment, 'error': error_msg})
-        comment.content = request.POST.get('content')
-        comment.rating = request.POST.get('rating')
-        comment.date = now()
-        comment.save()
-        return redirect(reverse('train_detail', kwargs={'train_id': comment.train.id}))
-      else:
-        error_msg = 'Invalid values'
-        return render(request, 'comment/edit_comment.html', {'form': form, 'comment': comment, 'error': error_msg})
+        form = CommentForm(request.POST)
+        comment = get_object_or_404(Comment, id=pk)
+        if form.is_valid():
+            if not comment.user == request.user:
+                error_msg = "You are not allowed to edit this comment, because it does not belong to you"
+                return render(request, 'comment/edit_comment.html', {'form': form, 'comment': comment, 'error': error_msg})
+            comment.content = request.POST.get('content')
+            comment.rating = request.POST.get('rating')
+            comment.date = now()
+            comment.save()
+            return redirect(reverse('train_detail', kwargs={'train_id': comment.train.id}))
+        else:
+            error_msg = 'Invalid values'
+            return render(request, 'comment/edit_comment.html', {'form': form, 'comment': comment, 'error': error_msg})
 
     else:
-      comment = get_object_or_404(Comment, id=pk)
-      comment_dict = {'content': comment.content, 'rating': comment.rating}
-      form = CommentForm(comment_dict)
-      return render(request, 'comment/edit_comment.html', {'form': form, 'comment': comment})
+        comment = get_object_or_404(Comment, id=pk)
+        comment_dict = {'content': comment.content, 'rating': comment.rating}
+        form = CommentForm(comment_dict)
+        return render(request, 'comment/edit_comment.html', {'form': form, 'comment': comment})
 
 
 class CommentDelete(DeleteView):
@@ -78,18 +77,13 @@ class CommentDelete(DeleteView):
 def signup(request):
     error_message = ''
     if request.method == 'POST':
-        # This is how to create a 'user' form object
-        # that includes the data from the browser
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            # This will add the user to the database
             user = form.save()
-            # This is how we log a user in via code
             login(request, user)
             return redirect('index')
         else:
             error_message = 'Invalid sign up - try again'
-    # A bad POST or a GET request, so render signup.html with an empty form
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
@@ -105,3 +99,15 @@ def add_comment(request, train_id):
         comment.save()
 
         return redirect('train_detail', train_id=train_id)
+
+def journey_index(request):
+    journeys = Journey.objects.all()
+    return render(request, 'journey/journey.html', {
+        'journeys': journeys
+    })
+
+def journey_detail(request, journey_id):
+    journey = get_object_or_404(Journey, id=journey_id)
+    stops = journey.route.stationorder_set.all()
+
+    return render(request, 'journey/journey_detail.html', {'journey': journey, "stops": stops})
