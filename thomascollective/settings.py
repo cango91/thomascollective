fc6c144da42b990160ba5689d8390248a02c7ebd
cango91 <can.gologlu@gmail.com>
2023-08-18 11:41:59 -0400

    Bugfix (or feature remove): everyone can only delete own comments

--- thomascollective/settings.py
+++ thomascollective/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['thethomascollective-71f5ee19c1ee.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['thethomascollective-71f5ee19c1ee.herokuapp.com']
 
 
 # Application definition
